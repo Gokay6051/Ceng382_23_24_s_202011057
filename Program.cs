955e63d9a8e02e6df69da1060f47ae43910e854c
Ahmet Gökay Ürkmez <gokay.urkmez@tigahealth.com>
2024-04-04 14:33:44 +0300

    menu deleted, program works without interaction.

--- Program.cs
+++ Program.cs
@@ -34,54 +34,24 @@ class Program
         {
             NumberHandling = JsonNumberHandling.AllowReadingFromString | JsonNumberHandling.WriteAsString
         });
-        /*
-        if(RoomData?.Rooms != null)
-        {
-            foreach(var room in RoomData.Rooms)
-            {
-                Console.WriteLine($"Room Id : {room.roomId} RoomName : {room.roomName} Capacity : {room.Capacity}");
-            }
-        }
-        */
-
         
-
-        ReservationHandler Reservations = new ReservationHandler(16, 70);
+        ReservationHandler Reservations = new ReservationHandler(7, 10);
 
         //Adding reservations
         TableHelper.CreateReservations(ref Reservations, ref RoomData);
 
-        int choice;
-        while (true)
-        {
-            TableHelper.PrintMenu();
-            if (int.TryParse(Console.ReadLine(), out choice))
-            {
-                Console.Clear();
-                if(choice < 0 || choice > 3){
-                    Console.WriteLine("Please enter between 0 and 3");
-                }
-                if(choice == 0){
-                    Console.WriteLine("The program is over!");
-                    break;
-                }
-                if(choice == 1){
-                    TableHelper.ViewReservations(ref Reservations);
-                }
-                if(choice == 2){
-                    TableHelper.MakeReservation(ref Reservations, ref RoomData);
-                }
-                if(choice == 3){
-                    TableHelper.DeleteReservation(ref Reservations);
-                }
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Invalid input. Please enter a number.");
-            }
-        }
+        
+        TableHelper.ViewReservations(ref Reservations);
 
+        TableHelper.DeleteReservation(ref Reservations);
+
+        Console.WriteLine("");
+        Console.WriteLine("");
+        Console.WriteLine("After deleted some reservations");
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+        TableHelper.ViewReservations(ref Reservations);
         return 0;
     }
 }
--- TableHelper.cs
+++ TableHelper.cs
@@ -13,7 +13,7 @@ public static class TableHelper
     }
     
 
-    public static void PrintTable(string[,] table)
+    public static void PrintTable(Reservation[,] table)
     {
         string[] daysOfWeek = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
 
@@ -24,7 +24,10 @@ public static class TableHelper
             Console.Write($" {daysOfWeek[i]} |");
             for (int j = 0; j < 10; j++)
             {
-                Console.Write($"{table[i, j].PadRight(7)}|");
+                if(table[i, j] == null)
+                    Console.Write("   -   |");
+                else
+                    Console.Write($"{table[i, j].Room.roomName.PadRight(7)}|");
             }
             Console.WriteLine();
             Console.WriteLine("-----+-------+-------+-------+-------+-------+-------+--------+------+-------+-------+");
@@ -118,31 +121,8 @@ public static class TableHelper
 
 
     public static void ViewReservations(ref ReservationHandler Reservations)
-    {
-        int room;
-
-        Console.WriteLine("Please select a room:");
-        Console.WriteLine("1 - A-101");
-        Console.WriteLine("2 - A-102");
-        Console.WriteLine("3 - A-103");
-        Console.WriteLine("4 - A-104");
-        Console.WriteLine("5 - A-105");
-        Console.WriteLine("6 - A-106");
-        Console.WriteLine("7 - A-107");
-        Console.WriteLine("8 - A-108");
-        Console.WriteLine("9 - A-109");
-        Console.WriteLine("10 - A-110");
-        Console.WriteLine("11 - A-111");
-        Console.WriteLine("12 - A-112");
-        Console.WriteLine("13 - A-113");
-        Console.WriteLine("14 - A-114");
-        Console.WriteLine("15 - A-115");
-        Console.WriteLine("16 - A-116");
-
-        int.TryParse(Console.ReadLine(), out room);
-        Console.Clear();        
-
-        Reservations.DisplayWeekSchedule(room-1);
+    {  
+        Reservations.DisplayWeekSchedule();
     }
 
     public static void CreateReservations(ref ReservationHandler Reservations, ref RoomData RoomData)
@@ -166,7 +146,6 @@ public static class TableHelper
         Reservations.AddReservation("Şeyma", 14, 0, 3, RoomData.Rooms[14]);
         Reservations.AddReservation("Can", 0, 5, 1, RoomData.Rooms[0]);
         Reservations.AddReservation("Deniz", 1, 6, 8, RoomData.Rooms[1]);
-        Reservations.AddReservation("Ceren", 2, 6, 4, RoomData.Rooms[2]);
         Reservations.AddReservation("Hüseyin", 3, 3, 2, RoomData.Rooms[3]);
         Reservations.AddReservation("Gizem", 4, 2, 9, RoomData.Rooms[4]);
         Reservations.AddReservation("Burak", 5, 1, 7, RoomData.Rooms[5]);
@@ -180,9 +159,6 @@ public static class TableHelper
         Reservations.AddReservation("Berk", 13, 0, 9, RoomData.Rooms[13]);
         Reservations.AddReservation("Şeyma", 14, 6, 7, RoomData.Rooms[14]);
         Reservations.AddReservation("Can", 0, 5, 5, RoomData.Rooms[0]);
-        Reservations.AddReservation("Deniz", 1, 6, 1, RoomData.Rooms[1]);
-        Reservations.AddReservation("Ceren", 2, 6, 8, RoomData.Rooms[2]);
-        Reservations.AddReservation("Hüseyin", 3, 3, 4, RoomData.Rooms[3]);
         Reservations.AddReservation("Gizem", 4, 2, 2, RoomData.Rooms[4]);
         Reservations.AddReservation("Burak", 5, 1, 9, RoomData.Rooms[5]);
         Reservations.AddReservation("Merve", 6, 0, 7, RoomData.Rooms[6]);
@@ -193,7 +169,6 @@ public static class TableHelper
         Reservations.AddReservation("Oğuz", 11, 2, 6, RoomData.Rooms[11]);
         Reservations.AddReservation("Nazlı", 12, 1, 4, RoomData.Rooms[12]);
         Reservations.AddReservation("Berk", 13, 0, 2, RoomData.Rooms[13]);
-        Reservations.AddReservation("Şeyma", 14, 6, 9, RoomData.Rooms[14]);
 
         Console.Clear();
     }
@@ -201,84 +176,16 @@ public static class TableHelper
 
 
     public static void DeleteReservation(ref ReservationHandler Reservations)
-    {
-        int room;
-        int day;
-        int time;
-
-        while(true){
-            Console.WriteLine("Please select a room:");
-            Console.WriteLine("1 - A-101");
-            Console.WriteLine("2 - A-102");
-            Console.WriteLine("3 - A-103");
-            Console.WriteLine("4 - A-104");
-            Console.WriteLine("5 - A-105");
-            Console.WriteLine("6 - A-106");
-            Console.WriteLine("7 - A-107");
-            Console.WriteLine("8 - A-108");
-            Console.WriteLine("9 - A-109");
-            Console.WriteLine("10 - A-110");
-            Console.WriteLine("11 - A-111");
-            Console.WriteLine("12 - A-112");
-            Console.WriteLine("13 - A-113");
-            Console.WriteLine("14 - A-114");
-            Console.WriteLine("15 - A-115");
-            Console.WriteLine("16 - A-116");
-
-            int.TryParse(Console.ReadLine(), out room);
-            Console.Clear();
-
-            if(room < 1 || room > 16){
-                    Console.WriteLine("Please enter between 1 and 16");
-                    continue;
-            }
-            break;
-        }
-
-        while(true){
-            Console.WriteLine("Please select a day:");
-            Console.WriteLine("1 - Mon");
-            Console.WriteLine("2 - Tue");
-            Console.WriteLine("3 - Wed");
-            Console.WriteLine("4 - Thu");
-            Console.WriteLine("5 - Fri");
-            Console.WriteLine("6 - Sat");
-            Console.WriteLine("7 - Sun");
-
-            int.TryParse(Console.ReadLine(), out day);
-            Console.Clear();
-
-            if(day < 1 || day > 7){
-                    Console.WriteLine("Please enter between 1 and 7");
-                    continue;
-            }
-            break;
-        }
-        
-        while(true){
-            Console.WriteLine("Please select time:");
-            Console.WriteLine("1 - 10.00");
-            Console.WriteLine("2 - 11.00");
-            Console.WriteLine("3 - 12.00");
-            Console.WriteLine("4 - 13.00");
-            Console.WriteLine("5 - 14.00");
-            Console.WriteLine("6 - 15.00");
-            Console.WriteLine("7 - 16.00");
-            Console.WriteLine("8 - 17.00");
-            Console.WriteLine("9 - 18.00");
-            Console.WriteLine("10 - 19.00");
-
-            int.TryParse(Console.ReadLine(), out time);
-            Console.Clear();
-
-            if(time < 1 || time > 10){
-                    Console.WriteLine("Please enter between 1 and 10");
-                    continue;
-            }
-            break;
-        }
-
-        Reservations.DeleteReservation(room-1, day-1, time-1);
+    {   
+        Reservations.DeleteReservation(4, 2, 9);
+        Reservations.DeleteReservation(5, 1, 7);
+        Reservations.DeleteReservation(6, 0, 5);
+        Reservations.DeleteReservation(7, 6, 3);
+        Reservations.DeleteReservation(8, 5, 0);
+        Reservations.DeleteReservation(9, 4, 8);
+        Reservations.DeleteReservation(10, 3, 6);
+        Reservations.DeleteReservation(11, 2, 4);
+        Reservations.DeleteReservation(12, 1, 2);
     }
 
 }
