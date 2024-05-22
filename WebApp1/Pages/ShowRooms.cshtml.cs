22f969ba64b514c88ab733cfb421167355e92e2b
Ahmet Gökay Ürkmez <gokay.urkmez@tigahealth.com>
2024-05-22 04:07:40 +0300

    WebApp1 project coppy paste because, merge Auth process causes Conflict

---
+++ WebApp1/Pages/ShowRooms.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Pages
+{
+    public class ShowRoomsModel : PageModel
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ShowRoomsModel(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<Room> Rooms { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Rooms = await _dbContext.Rooms.ToListAsync();
+            return Page();
+        }
+    }
+}
